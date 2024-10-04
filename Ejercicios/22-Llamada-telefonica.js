9797c501a07c7931e242a93b753c2794d1234faa
Guillermo Garrido <gnilleuni@gmail.com>
2024-10-04 12:25:56 -0500

    22

---
+++ Ejercicios/22-Llamada-telefonica.js
@@ -0,0 +1,28 @@
+//EJERCICIO 22
+
+//Escribir un programa que muestre el pago de una llamada telefónica sabiendo que cada
+//minuto cuesta $355 pesos y un IVA 20%.
+
+
+
+   // Valores
+let minutos = 10; // Duración de la llamada en minutos
+let costoPorMinuto = 355; // Costo por minuto en pesos
+let iva = 0.20; // IVA del 20%
+
+
+
+   // Función para calcular el costo total de la llamada
+function calcularCostoLlamada(minutos, costoPorMinuto, iva) {
+    let subtotal = minutos * costoPorMinuto;
+    let totalIva = subtotal * iva;
+    let total = subtotal + totalIva;
+    return total;
+}
+
+
+   // Cálculo y resultado
+let totalPagar = calcularCostoLlamada(minutos, costoPorMinuto, iva);
+console.log(`El costo total de la llamada es: $${totalPagar.toFixed(2)} pesos`);
+
+
