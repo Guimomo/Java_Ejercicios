96d3bdd1951d448c223d09a72cc48097ab670575
Guillermo Garrido <gnilleuni@gmail.com>
2024-10-05 20:38:49 -0500

    23

--- Ejercicios/22-Llamada-telefonica.js
+++ Ejercicios/22-Llamada-telefonica.js
@@ -6,14 +6,15 @@
 
 
    // Valores
-let minutos = 10; // Duración de la llamada en minutos
-let costoPorMinuto = 355; // Costo por minuto en pesos
-let iva = 0.20; // IVA del 20%
+let minutos = parseFloat(prompt("Cuantos minutos hablo?")); // Duración de la llamada en minutos
+const costoPorMinuto = 355; // Costo por minuto en pesos
+const iva = 0.20; // IVA del 20%
 
 
 
    // Función para calcular el costo total de la llamada
 function calcularCostoLlamada(minutos, costoPorMinuto, iva) {
+   
     let subtotal = minutos * costoPorMinuto;
     let totalIva = subtotal * iva;
     let total = subtotal + totalIva;
